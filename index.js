999c0b950206ac8b95c134c4d02989be82386aa6
Steve <steve@roaringdevelopment.com>
2018-06-14 13:58:49 -0600

    new leo config

--- index.js
+++ index.js
@@ -1,2 +1,57 @@
 "use strict";
-module.exports = require("./lib/build.js").dynamicBuild(process.cwd());
+const merge = require('lodash.merge');
+let system = process.env.NODE_ENV;
+let isLocal = process.env.LEO_LOCAL || 'false';
+async function cacheSetting(key, promise, time = null) {
+	if (!(key in settingCache)) {
+		settingCache[key] = await promise;
+	}
+
+	return settingCache[key];
+}
+
+let config = {
+	env: system,
+	bootstrap: function(params) {
+		if (typeof params == "string") {
+			params = require(params);
+		}
+		//Lets check if they have a global
+		if ("_global" in params) {
+			if (typeof params._global == "function") {
+				merge(config, params._global.call(config));
+			} else {
+				merge(config, params._global);
+			}
+		}
+		if (system in params) {
+			if (typeof params[system] == "function") {
+				merge(config, params[system].call(config));
+			} else {
+				merge(config, params[system]);
+			}
+		}
+		//Lets check if they have a global
+		if (isLocal === 'true' && "_local" in params) {
+			if (typeof params._local == "function") {
+				merge(config, params._local.call(config));
+			} else {
+				merge(config, params._local);
+			}
+		}
+		global.leosdk = config.leosdk;
+		return module.exports;
+	}
+};
+module.exports = new Proxy(config, {
+	get: function(target, propKey) {
+		const orig = target[propKey];
+		if (propKey == "bootstrap") {
+			return orig;
+		} else if (orig && typeof orig == "function") {
+			return orig.call(config, cacheSetting.bind(config, propKey));
+		} else {
+			return orig;
+		}
+	}
+});
